dbbc989958bfdbb6dcfc7f6a82e024a486b52550
Viktor Malmedal <viktor@malmedal.dev>
2023-03-11 15:45:38 +0100

    feat: register ws plugin

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -2,6 +2,8 @@ import fastify from "fastify";
 import { drizzleFastifyPlugin } from "./db/drizzle-connector-plugin";
 import { env } from "./env";
 import { helloRoutes } from "./routes/test";
+import ws from "@fastify/websocket";
+import { websocketPlugin } from "./ws/websocket-plugin";
 
 export const app = fastify();
 
@@ -9,7 +11,18 @@ app.register(drizzleFastifyPlugin, {
   connectionString: env.POSTGRES_CONNECTION_STRING,
 });
 
+app.register(ws, {
+  errorHandler(error, connection, request, reply) {
+    console.log(error);
+    connection.destroy();
+  },
+  options: {
+    maxPayload: 1024 * 1024,
+  },
+});
+
 app.register(helloRoutes);
+app.register(websocketPlugin);
 
 app.get("/*", async (_request, _reply) => {
   return { hello: "world" };
