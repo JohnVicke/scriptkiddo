58d5f9779e21082bad4b12733aad2692edba469d
Viktor Malmedal <viktor@malmedal.dev>
2023-03-19 16:55:11 +0100

    add server router

---
+++ apps/api/src/modules/server/server.router.ts
@@ -0,0 +1,12 @@
+import { FastifyInstance } from "fastify";
+import { createServerHandler } from "./server.controller";
+import { createServerSchema } from "./server.schema";
+
+export async function serverRouter(fastify: FastifyInstance) {
+  fastify.route({
+    method: "POST",
+    url: "/",
+    handler: createServerHandler,
+    schema: createServerSchema,
+  });
+}
