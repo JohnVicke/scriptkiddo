c1dcb568ca2c34843df923e00e4cdb4acf2f152d
Viktor Malmedal <viktor@malmedal.dev>
2023-03-19 11:20:41 +0100

    cleanup

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,35 @@
-import { server } from "./app";
+import { createServer } from "./utils/create-server";
 import { env } from "./env";
 
-server.listen({ port: env.PORT }, (err, address) => {
-  if (err) {
-    server.log.error(err);
-    process.exit(1);
-  }
-  console.log(`Server listening at ${address}`);
+const signals = ["SIGINT", "SIGTERM", "SIGHUP"] as const;
+
+async function main() {
+  const server = await createServer();
+  server.listen({ port: env.PORT }, (err, address) => {
+    if (err) {
+      server.log.error(err);
+      process.exit(1);
+    }
+  });
+
+  signals.forEach((signal) => {
+    process.on(signal, () => graceFullShutdown({ signal, server }));
+  });
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
 });
+
+async function graceFullShutdown({
+  signal,
+  server,
+}: {
+  signal: (typeof signals)[number];
+  server: Awaited<ReturnType<typeof createServer>>;
+}) {
+  console.log(`🛑 [${signal}] Graceful shutdown...`);
+  await server.close();
+  console.log("🛑 Graceful shutdown complete.");
+}
