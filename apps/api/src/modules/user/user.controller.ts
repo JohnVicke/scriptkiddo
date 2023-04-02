fdafc63ad5a55e0aab69622cd1363dc536626241
Viktor Malmedal <viktor@malmedal.dev>
2023-04-02 09:11:52 +0200

    refactor: use SKRouteHandler

--- apps/api/src/modules/user/user.controller.ts
+++ apps/api/src/modules/user/user.controller.ts
@@ -1,19 +1,17 @@
-import { FastifyReply, FastifyRequest } from "fastify";
+import { SKRouteHandler } from "$types/create-route-handler";
 import { CreateUserBody, GetUserParams } from "./user.schema";
 import { createUser, getUser } from "./user.service";
 
-export async function createUserHandler(
-  request: FastifyRequest<{ Body: CreateUserBody }>,
-  reply: FastifyReply,
-) {
-  const user = await createUser(request.body);
-  return reply.code(201).send(user);
-}
+export const createUserHandler: SKRouteHandler<{ Body: CreateUserBody }> = () => {
+  return async (request, reply) => {
+    const user = await createUser(request.body);
+    return reply.code(201).send(user);
+  };
+};
 
-export async function getUserHandler(
-  request: FastifyRequest<{ Params: GetUserParams }>,
-  reply: FastifyReply,
-) {
-  const user = await getUser(request.params.id);
-  return reply.send(user);
-}
+export const getUserHandler: SKRouteHandler<{ Params: GetUserParams }> = () => {
+  return async (request, reply) => {
+    const user = await getUser(request.params.id);
+    return reply.send(user);
+  };
+};
