bd148f7b1f28b3d26a65c430c842dd7aa5c3bed1
Viktor Malmedal <viktor@malmedal.dev>
2023-04-02 09:10:20 +0200

    add dates to user model

--- apps/api/src/db/user.model.ts
+++ apps/api/src/db/user.model.ts
@@ -1,8 +1,10 @@
-import { InferModel, pgTable, serial, text } from "drizzle-orm/pg-core";
+import { InferModel, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
   id: serial("id").primaryKey(),
   username: text("username").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
 
 export type User = InferModel<typeof user>;
