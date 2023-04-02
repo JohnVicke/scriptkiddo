b999e76f753622d6f9ed862dba20c0886a59b8d3
Viktor Malmedal <viktor@malmedal.dev>
2023-04-02 09:10:57 +0200

    type alias

--- apps/api/src/modules/server/server.service.ts
+++ apps/api/src/modules/server/server.service.ts
@@ -1,4 +1,4 @@
-import { InsertServer } from "../../db/server.model";
+import { InsertServer } from "$db/server.model";
 
 export async function createServer(serverInput: InsertServer) {
   return { id: 1, ...serverInput };
